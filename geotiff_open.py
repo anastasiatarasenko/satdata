1b43cfd7582ce3438fc5677c386d09ba692983cc
anastasiatarasenko <tad.ocean@gmail.com>
2017-08-29 10:50:50 +0300

    Created geotiff_open.py
    Simple script to open geotiff files with one or several bands; get the arrays of latitudes and longitudes

---
+++ geotiff_open.py
@@ -0,0 +1,47 @@
+%pylab inline
+
+import numpy as np
+import numpy.ma as ma
+import gdal
+
+def readFile(filename, nbofbands=1):
+    filehandle = gdal.Open(filename)
+    if filehandle is None:
+      print ('Unable to open ', filename)
+      sys.exit(1)
+    
+    geotransform = filehandle.GetGeoTransform()
+    geoproj = filehandle.GetProjection()
+    print (geotransform)
+
+    xsize = filehandle.RasterXSize
+    ysize = filehandle.RasterYSize
+    
+    band_data=np.zeros((ysize, xsize, filehandle.RasterCount), dtype=np.float64)
+    
+    print ("[ RASTER BAND COUNT ]: ", filehandle.RasterCount)
+    for band in range(filehandle.RasterCount):
+      band +=1
+      print ("[ GETTING BAND ]: ", band)
+      scrband = filehandle.GetRasterBand(band)
+
+      if scrband is None:
+        continue
+      band_data[:, :, band-1] = scrband.ReadAsArray()
+
+    return xsize,ysize, geotransform, geoproj, band_data
+    
+    
+def get_latlon(band_data, geotransform, nth_point=10):
+    # geotransform is a gdal way of describing the data grid, see http://www.gdal.org/gdal_tutorial.html
+    # nth_point is needed to artificially decrease high spatial resolution of 40-m Sentinel-1 images 
+    # eg: 40*10th point => resolution of ~400 m
+   
+    Xpixel, Yline = np.meshgrid(np.arange(0, band_data.shape[1], nth_point), 
+                                np.arange(1, band_data.shape[0], nth_point))
+    lon_rastr = geotransform[0] + Xpixel*geotransform[1] + Yline*geotransform[2]
+    lat_rastr = geotransform[3] + Xpixel*geotransform[4] + Yline*geotransform[5]
+    return lon_rastr, lat_rastr
+    
+    
+    
